4f9d017509089d3db9d26466a110bdea1a11e5e6
Adi Sulistiono <sulistiono.adi@gmail.com>
2020-08-31 13:11:30 +0700

    Fixing Audit for method setAsDelete and setActive

--- src/main/java/com/sulistionoadi/ngoprek/common/audit/DataAuditorAspect.java
+++ src/main/java/com/sulistionoadi/ngoprek/common/audit/DataAuditorAspect.java
@@ -3,14 +3,17 @@ package com.sulistionoadi.ngoprek.common.audit;
 import static com.sulistionoadi.ngoprek.common.constant.ErrorCode.*;
 
 import java.beans.Statement;
+import java.util.Arrays;
 import java.util.Date;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.aop.framework.ReflectiveMethodInvocation;
+import org.springframework.aop.aspectj.MethodInvocationProceedingJoinPoint;
 
 import com.sulistionoadi.ngoprek.common.exception.CommonRuntimeException;
 import com.sulistionoadi.ngoprek.common.utils.SessionUtils;
@@ -73,43 +76,42 @@ public class DataAuditorAspect {
 		}
 	}
 	
-	@Before("execution(* *.setAsDelete(..)) && within(*..service.impl.*)")
-	public void beforeDelete(JoinPoint joinPoint) {
-		log.debug("Before Delete {} ", joinPoint.getTarget());
+	
+	@Around("execution(* *.setAsDelete(..)) && within(*..service.impl.*)")
+	public void beforeDelete(ProceedingJoinPoint joinPoint) throws Throwable {
+		Object arguments[] = joinPoint.getArgs();
+		log.debug("Before setAsDelete({}) ", Arrays.toString(arguments));
 
 		String principal = SessionUtils.getPrincipalName();
-		if(joinPoint.getArgs().length < 2) {
+		if(arguments.length < 2) {
 			throw new CommonRuntimeException(RC_INVALID_PARAMETER, "Number of Argument minimum is 2");
 		}
 		
-		try {
-			if (joinPoint instanceof ReflectiveMethodInvocation) {
-                ReflectiveMethodInvocation invocation = (ReflectiveMethodInvocation) joinPoint;
-                joinPoint.getArgs()[1] = principal;
-                invocation.setArguments(joinPoint.getArgs());
-            }
-		} catch (Exception e) {
-			throw new CommonRuntimeException("Failed inject second arguments", e);
+		if (joinPoint instanceof MethodInvocationProceedingJoinPoint) {
+			MethodInvocationProceedingJoinPoint invoke = (MethodInvocationProceedingJoinPoint) joinPoint;
+			arguments[1] = principal;
+			
+			log.debug("Set new Arguments for method setAsDelete({}) ", Arrays.toString(arguments));
+			invoke.proceed(arguments);
 		}
 	}
 	
-	@Before("execution(* *.setActive(..)) && within(*..service.impl.*)")
-	public void beforeSetActive(JoinPoint joinPoint) throws CommonRuntimeException {
-		log.debug("Before setActive {} ", joinPoint.getTarget());
+	@Around("execution(* *.setActive(..)) && within(*..service.impl.*)")
+	public void beforeSetActive(ProceedingJoinPoint joinPoint) throws Throwable {
+		Object arguments[] = joinPoint.getArgs();
+		log.debug("Before setActive({}) ", Arrays.toString(arguments));
 
 		String principal = SessionUtils.getPrincipalName();
-		if(joinPoint.getArgs().length < 3) {
+		if(arguments.length < 3) {
 			throw new CommonRuntimeException(RC_INVALID_PARAMETER, "Number of Argument minimum is 3");
 		}
 		
-		try {
-			if (joinPoint instanceof ReflectiveMethodInvocation) {
-                ReflectiveMethodInvocation invocation = (ReflectiveMethodInvocation) joinPoint;
-                joinPoint.getArgs()[2] = principal;
-                invocation.setArguments(joinPoint.getArgs());
-            }
-		} catch (Exception e) {
-			throw new CommonRuntimeException("Failed inject third arguments", e);
+		if (joinPoint instanceof MethodInvocationProceedingJoinPoint) {
+			MethodInvocationProceedingJoinPoint invoke = (MethodInvocationProceedingJoinPoint) joinPoint;
+			arguments[2] = principal;
+			
+			log.debug("Set new Arguments for method setActive({}) ", Arrays.toString(arguments));
+			invoke.proceed(arguments);
 		}
 	}
 	
